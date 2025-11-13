4b95099bdf6ed431fbac726ecfbc7e4e03bd3ecd
SRIHARI SAMARTH <112471883+sriharisamarth@users.noreply.github.com>
2025-11-13 11:29:33 +0530

    Add files via upload

---
+++ Assignments/Assignment_2025_11_07_Check_Century_Year.py
@@ -0,0 +1,9 @@
+x=int(input("enter a year: \n"))
+
+
+if x%100==0:
+    print("Its a century year")
+
+else:
+    print("Its not a century year")
+      
---
+++ Assignments/Assignment_2025_11_12_while_loop.py
@@ -0,0 +1,26 @@
+print(10*"*")
+x=int(input("Enter a number which is upto:\n"))
+print("Numbers from your desired number")
+i = 1
+while(i<=x):
+    print(i)
+    i+= 1
+
+
+print(10*"*")
+j=int(input("Enter a number which is upto:\n"))
+print("Even numbers between your desired number")
+f = 0
+while(f<=j):
+    print(f)
+    f+= 2
+
+
+print(10*"*")
+z=int(input("Enter a number which is upto:\n"))
+print("Odd numbers between 1 and your desired number")
+
+g = 1
+while(g<=z):
+    print(g)
+    g+=2
